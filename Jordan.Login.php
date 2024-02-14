937d03425256b6aa64ef511ea759f41d0d7efbdf
pzjpzy <103748603+pzjpzy@users.noreply.github.com>
2024-02-14 20:45:02 +0800

    Update and rename Jordan.Login-html.html to Jordan.Login.php

--- Jordan.Login.php
+++ Jordan.Login.php
@@ -4,22 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tree Gaia</title>
-    <link rel="stylesheet" href="login-css.css">
+    <link rel="stylesheet" href="Jordan.login-css.css">
 </head>
 <body>
     
     
     <div class="wrapper">
-        <form action="">
+        <form action="buffer.php" method="post">
             <h1>Login</h1>
-
+            
             <div class="input-box">
-                <input type="text" 
+                <input type="text" name="username"
                 placeholder="Username" required>
             </div>
 
             <div class="input-box">
-                <input type="password" 
+                <input type="password" name="password"
                 placeholder="password" required>
             </div>
 
@@ -29,7 +29,7 @@
                 <a href="#">Forgot password?</a>
             </div>
 
-            <button type="submit" class="btn">Login</button>
+            <input type="submit" name="loginbtn" class="btn" value="Login">
 
             <div class="register-link"></div>
                 <p>Don't have an account? <a href="#">register</a></p>
