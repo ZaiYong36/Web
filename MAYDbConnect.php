29c858039d96aa2e26e243be33b06fcebf296222
MAY963 <142193849+MAY963@users.noreply.github.com>
2024-02-14 19:05:00 +0800

    Update MAYDbConnect.php

--- MAYDbConnect.php
+++ MAYDbConnect.php
@@ -1,20 +1,14 @@
 <?php
     $db_server="localhost";
     $db_user="root";
-    $db_pass=" ";
+    $db_pass="";
     $db_name="adminrecords2";
-    $conn="";
-
-    try{
-        $conn=mysqli_connect($db_server,
-                            $db_user,
-                            $dp_pass,
-                            $db_name);
+    
+    $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+    
+    if(!$conn){
+        die("Connection Failed:". mysqli_connect_error());
+    }else{
+        echo "You are now connected!";
     }
-    catch(mysqli_sql_exception) {
-        echo "You are not connected";
-    }
-    /*if($conn){
-        echo"You are connected";
-    }*/
 ?>
